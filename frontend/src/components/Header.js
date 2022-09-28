cd1a9059e26afa1115f077a7690d32d4885dfd76
Gianmarco G <giangarof98@gmail.com>
2022-09-27 20:34:10 -0400

    bootstrap setup

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Container, 
+        Navbar, 
+        Nav, 
+        } from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">Zagara</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="ms-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                            >
+                            <Nav.Link href="/cart">Cart <i className="fas fa-shopping-cart"></i> </Nav.Link>
+                            <Nav.Link href="/signin">Sign In <i className="fas fa-user"></i> </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
