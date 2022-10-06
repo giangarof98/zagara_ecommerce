aec60e354076ed310ec4007df830d5cc789dfd60
Gianmarco G <giangarof98@gmail.com>
2022-10-06 09:53:36 -0400

    backend; controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,24 +1,12 @@
 import express from 'express';
-import asyncHandler from "express-async-handler"
-import Product from '../model/productModel.js';
 import mongoose from 'mongoose';
+import {getProducts,getOne} from '../controllers/productController.js';
 const router = express.Router();
 
 //Fetch all the products
-router.get('/', asyncHandler(async (req,res) => {
-    const products = await Product.find({});
-    res.json(products);
-}));
+router.route('/').get(getProducts)
 
 //Fetch product by id
-router.get('/:id', asyncHandler( async (req,res) => {
-    const product = await Product.findById(req.params.id)
-    if(product){
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}));
+router.route('/:id').get(getOne);
 
 export default router;
