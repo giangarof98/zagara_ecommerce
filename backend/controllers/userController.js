9e3be0f7a054f20f12c89ffb57c1f69b4f1895ef
Gianmarco G <giangarof98@gmail.com>
2022-10-06 21:09:01 -0400

    registrate user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,8 +41,38 @@ const getUserProfile = asyncHandler(async (req,res) => {
     }
 });
 
+const registerUser = asyncHandler(async(req,res) => {
+    const {name, email, password} = req.body;
+
+    const userExists = await User.findOne({email});
+
+    if(userExists){
+        res.status(400)
+        throw new Error(`User already exist with this email.`)
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else{
+        res.status(400)
+        throw new Error(`Invalid User data`)
+    }
+});
+
 export {
     authUser,
     getUserProfile,
+    registerUser,
 }
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import {authUser, getUserProfile} from '../controllers/userController.js';
+import {authUser, 
+        getUserProfile, 
+        registerUser} from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
+router.route('/').post(registerUser)
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile);
+
+
 
 export default router
