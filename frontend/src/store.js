edbd2b948d8ed39dd8612c5ead24b156dc629d66
Gianmarco G <giangarof98@gmail.com>
2022-10-07 12:40:28 -0400

    frontend; login store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,17 +3,23 @@ import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk';
 import {productListReducer, productDetailsReducer} from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers';
+import {userLoginReducer} from './reducers/userReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
+
 });
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
 
 let initialState = {
-    cartItems: cartItemsFromStorage
+    cartItems: cartItemsFromStorage,
+    userLogin: userInfoFromStorage
 };
 
 //const middleware = [thunk];
