e3934ee215d16575f877e0345ee6bc32b8862a28
Gianmarco G <giangarof98@gmail.com>
2022-10-12 21:19:35 -0400

    order controller && route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -19,6 +20,7 @@ app.use(express.json())
 
 app.use('/api/products', productRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/order', orderRoutes);
 
 //custom middleware
 
