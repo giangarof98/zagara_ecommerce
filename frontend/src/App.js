0af8a0ca055dfed19e612fc66d2b3822540f670f
Gianmarco G <giangarof98@gmail.com>
2022-10-20 12:32:17 -0400

    admin role; product list screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screen/PlaceOrderScreen';
 import OrderScreen from './screen/OrderScreen';
 import UserListScreen from './screen/UserListScreen';
 import UserEditScreen from './screen/UserEditScreen';
+import ProductListScreen from './screen/ProductListScreen';
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
             <Routes>
               <Route path='/admin/user/:id/edit' element={<UserEditScreen/>} />
               <Route path='/admin/userlist' element={<UserListScreen/>} />
+              <Route path='/admin/productlist' element={<ProductListScreen/>} />
               <Route path='/placeorder' element={<PlaceOrderScreen/>} />
               <Route path='/payment' element={<PaymentScreen/>} />
               <Route path='/shipping' element={<ShippingScreen/>} />
