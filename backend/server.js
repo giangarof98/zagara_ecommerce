b8537ae8946b7aa2ae7cefab7c2bcef286625e66
Gianmarco G <giangarof98@gmail.com>
2022-10-24 11:43:10 -0400

    morgan added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 import path from 'path'
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -13,6 +14,10 @@ dotenv.config()
 connectDB()
 
 const app = express();
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
